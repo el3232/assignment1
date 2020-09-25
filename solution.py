7c798f53dc7af8cefcb2dc3d6c207e249b9c2422
Elina Leshchenko <el3232@nyu.edu>
2020-09-24 21:54:00 -0400

    solution file attempt 2

--- solution.py
+++ solution.py
@@ -4,31 +4,49 @@
 
 # attempting to commit to github
 
+
 def welcome_assignment_answers(question):
     # The student doesn't have to follow the skeleton for this assignment.
     # Another way to implement is using a "case" statements similar to C.
-    answer = "Please enter one of the 8 questions from 'Computer Networking Assignment 1'"
+    # answer = "Please enter one of the 8 questions from 'Computer Networking Assignment 1'"
+
+    # question 1
+    answer = None
     if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "No"
+        answer = "no"
+
+    # question 2
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "no"
         #  - while legitimate decryption requires a key, breaking " \
         # "encryption is possible by methods such as brute force or rainbow " \
         # "tables, and is technically possible though depending on how much data" \
         # "and what methods were used to encrypt it, might be impractical
+
+    # question 3
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "Yes"
+
+    # question 4
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "No"
+
+    # question 5
     elif question == "What is the MD5 hashing value to the following message: 'NYU" \
                      "Computer Networking' - Use MD5 hash generator and use the " \
                      "answer in your code":
         answer = "42b76fe51778764973077a5a94056724"
+
+    # question 6
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
+
+    # question 7
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to?" \
                      " - The answer should be a numeric number":
-        answer = 1
+        answer = 5
+
+    # question 8
     elif question == "What layer from the TCP/IP model the protocol TCP belongs to?" \
                      " - The answer should be a numeric number":
         answer = 2
